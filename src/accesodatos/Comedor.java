3d5059361dd57f9dbbb542eec45e4090ff54a8e6
santi <sirlocus66@gmail.com>
2020-11-22 01:09:46 +0100

    Guarda nodo

--- src/accesodatos/Comedor.java
+++ src/accesodatos/Comedor.java
@@ -4,78 +4,125 @@ import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.modules.XPathQueryService;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class Comedor extends JFrame{
-    private JPanel Principal;
-    private JTextField jtCliente;
-    private JComboBox<String> cbCamareros;
-    private JTextField tjtFecha;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
-    private JLabel lImporte;
-    private JPanel jpBotonera;
-    private JButton bGuardar;
-    private JButton bLimpiar;
+public class Comedor extends JFrame {
+  private JPanel Principal;
+  private JTextField jtCliente;
+  private JComboBox<String> cbCamareros;
+  private JTextField tjtFecha;
+  private JComboBox<Object> cbPrimero;
+  private JComboBox<Object> cbSegundo;
+  private JComboBox<Object> cbPostre;
+  private JLabel lImporte;
+  private JPanel jpBotonera;
+  private JButton bGuardar;
+  private JButton bLimpiar;
 
-    public Comedor() {
+  private static float precioPrimero = 0;
+  private static float precioSegundo = 0;
+  private static float precioPostre = 0;
 
-        add(Principal);
+  public Comedor() {
 
-        setTitle("Comedor");
-        setResizable(false);
-        setSize(520, 300);
+    add(Principal);
+
+    setTitle("Comedor");
+    setResizable(false);
+    setSize(520, 300);
 
 
-        cbCamareros.addItem("Juanito");
-        cbCamareros.addItem("Merche");
+    ArrayList<String> camareros = new ArrayList<>() {{
+      add("Juanito");
+      add("Merche");
+    }};
 
-        bGuardar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+    cbCamareros.addItem(camareros.get(0));
+    cbCamareros.addItem(camareros.get(1));
 
-                String nombre = jtCliente.getText();
-                String fecha = tjtFecha.getText();
-
-                insertaFactura("pepe","03/11/2020","juanito","primero","segundo","postre","100");
-            }
-        });
+    for (Object primero : Main.primerosArray) {
+      cbPrimero.addItem(primero);
     }
 
-    //update insert <Factura><Fecha>03/12/2020</Fecha></Factura> into /Comandas/Cliente[@nombre = "Juan"]
-    //para insertar una factura aun cliente concreto
-
-    private void insertaFactura(String nombre,String fecha,String camarero,String primero,String segundo,String postre,String importe) {
-
-        //Caso concreto: sabemos cuáles son los nodos
-        String nuevaFactura = "<Cliente nombre = "+nombre+ ">" +
-                "<Factura>" +
-                    "<Fecha>"+fecha+"</Fecha>"+
-                    "<Camarero>"+camarero+"</Camarero>"+
-                    "<Primero>"+primero+"</Primero>"+
-                    "<Segundo>"+segundo+"</Segundo>"+
-                    "<Postre>"+postre+"</Postre>"+
-                    "<Importe>"+importe+"</Importe>"
-                +"</Factura>";
-
-        if (Utilidades.conectar() != null) {
-            try {
-                XPathQueryService servicio = (XPathQueryService) Utilidades.col.getService("XPathQueryService", "1.0");
-                System.out.printf("Inserto: %s \n", nuevaFactura);
-                //Consulta para insertar --> update insert ... into
-                ResourceSet result = servicio.query("update insert " + nuevaFactura + " into /departamentos");
-                Utilidades.col.close(); //borramos
-                System.out.println("Dep insertado.");
-            } catch (Exception e) {
-                System.out.println("Error al insertar empleado.");
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Error en la conexión. Comprueba datos.");
-        }
-
+    for (Object segundo : Main.segundosArray) {
+      cbSegundo.addItem(segundo);
     }
+
+    for (Object postre : Main.primerosArray) {
+      cbPostre.addItem(postre);
+    }
+
+    bGuardar.addActionListener(e -> {
+
+      String nombre = jtCliente.getText();
+      String fecha = tjtFecha.getText();
+      String camarero = camareros.get(cbCamareros.getSelectedIndex());
+      String primero = (String) Main.primerosArray[cbPrimero.getSelectedIndex()];
+      String segundo = (String) Main.segundosArray[cbSegundo.getSelectedIndex()];
+      String postre = (String) Main.postresArray[cbPostre.getSelectedIndex()];
+
+
+      insertaFactura(nombre, fecha, camarero, primero, segundo, postre,
+          String.valueOf(Main.primeros.get(primero) + Main.segundos.get(segundo) + Main.postres.get(postre)));
+    });
+
+    cbPrimero.addActionListener(e -> {
+
+      precioPrimero = Main.primeros.get((String) Main.primerosArray[cbPrimero.getSelectedIndex()]);
+
+      lImporte.setText(String.valueOf(precioPrimero + precioSegundo + precioPostre));
+    });
+
+    cbSegundo.addActionListener(e -> {
+
+      precioSegundo = Main.segundos.get((String) Main.segundosArray[cbSegundo.getSelectedIndex()]);
+
+      lImporte.setText(String.valueOf(precioPrimero + precioSegundo + precioPostre));
+
+    });
+    cbPostre.addActionListener(e -> {
+
+      precioPostre = Main.postres.get((String) Main.postresArray[cbPostre.getSelectedIndex()]);
+
+      lImporte.setText(String.valueOf(precioPrimero + precioSegundo + precioPostre));
+
+
+    });
+  }
+
+  //update insert <Factura><Fecha>03/12/2020</Fecha></Factura> into /Comandas/Cliente[@nombre = "Juan"]
+  //para insertar una factura aun cliente concreto
+
+  private void insertaFactura(String nombre, String fecha, String camarero, String primero, String segundo, String postre, String importe) {
+
+    //Caso concreto: sabemos cuáles son los nodos
+    String nuevaFactura =
+        "<Cliente nombre = \"" + nombre + "\">" +
+            "<Factura>" +
+            "<Fecha>" + fecha + "</Fecha>" +
+            "<Camarero>" + camarero + "</Camarero>" +
+            "<Primero>" + primero + "</Primero>" +
+            "<Segundo>" + segundo + "</Segundo>" +
+            "<Postre>" + postre + "</Postre>" +
+            "<Importe>" + importe + "</Importe>"
+            + "</Factura>" +
+            "</Cliente>";
+
+    if (Utilidades.conectar() != null) {
+      try {
+        XPathQueryService servicio = (XPathQueryService) Utilidades.col.getService("XPathQueryService", "1.0");
+        System.out.printf("Inserto: %s \n", nuevaFactura);
+        //Consulta para insertar --> update insert ... into
+        ResourceSet result = servicio.query("update insert " + nuevaFactura + " into /Comandas");
+        Utilidades.col.close(); //borramos
+        System.out.println("Dep insertado.");
+      } catch (Exception e) {
+        System.out.println("Error al insertar empleado.");
+        e.printStackTrace();
+      }
+    } else {
+      System.out.println("Error en la conexión. Comprueba datos.");
+    }
+
+  }
 }
--- src/accesodatos/Main.java
+++ src/accesodatos/Main.java
@@ -11,134 +11,186 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
+  //Se crea el menu contenido en Maps asociando el plato al precio Key Value
+  static Map<String, Float> primeros = Map.of(
+      "Sopa de huevo", 7.2f,
+      "Judías verdes", 12.3f,
+      "Berenjenas agridulces", 15.5f,
+      "Lentejas con calabaza", 8.7f
 
-    public static void main(String[] args) {
+  );
 
-        crearXml();
-        Utilidades.cargar_en_coleccion();
+  static Map<String, Float> segundos = Map.of(
+      "Trucha marinada", 25.2f,
+      "Bonito marinado", 32.3f,
+      "Fajitas de ternera", 18.5f,
+      "Lomo de cerdo", 29.7f
 
-        Comedor comedor = new Comedor();
-        comedor.setLocationRelativeTo(null);
-        comedor.setVisible(true);
+  );
+
+  static Map<String, Float> postres = Map.of(
+      "Trufas de cava", 25.2f,
+      "Crema catalana", 42.3f,
+      "Brownie de chocolate", 5.5f,
+      "Goxua", 39.7f
+
+  );
+
+  //Se transforma en un array de objetos con el objetivo de sacar un valor aleatorio de las keys del map
+  static Object[] primerosArray = primeros.keySet().toArray();
+  static Object[] segundosArray = segundos.keySet().toArray();
+  static Object[] postresArray = postres.keySet().toArray();
+
+
+  public static void main(String[] args) {
+
+    crearXml();
+    Utilidades.cargar_en_coleccion();
+
+    Comedor comedor = new Comedor();
+    comedor.setLocationRelativeTo(null);
+    comedor.setVisible(true);
+  }
+
+  /**
+   * Crea el fichero XML
+   */
+  private static void crearXml() {
+
+    //Lista con el contenido de los nodos de cada factura de cada cliente
+    List<Factura> facturas = new ArrayList<>();
+    //Lista con las lineas de cada factura
+    List<LineaTiket> lineas = new ArrayList<>();
+
+
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+    try {
+      DocumentBuilder builder = factory.newDocumentBuilder();
+      DOMImplementation implementation = builder.getDOMImplementation();
+      Document document = implementation.createDocument(null, "Comandas", null);
+      document.setXmlVersion("1.0");
+
+
+      //Se crean los tikects pedro tendra tres tikects
+      creaTikect(lineas, "03/02/2020", "Juanito");
+      creaTikect(lineas, "04/02/2020", "Merche");
+      creaTikect(lineas, "05/02/2020", "Juanito");
+
+      facturas.add(new Factura("pedro", lineas));
+      crearCliente(facturas, lineas, document);
+
+      lineas.clear();
+      facturas.clear();
+
+      creaTikect(lineas, "03/02/2020", "Juanito");
+      creaTikect(lineas, "04/02/2020", "Juanito");
+      creaTikect(lineas, "05/02/2020", "Juanito");
+      creaTikect(lineas, "06/02/2020", "Juanito");
+      creaTikect(lineas, "07/02/2020", "Merche");
+      creaTikect(lineas, "08/02/2020", "Juanito");
+
+      facturas.add(new Factura("Juan", lineas));
+      crearCliente(facturas, lineas, document);
+
+      lineas.clear();
+      facturas.clear();
+
+      creaTikect(lineas, "10/02/2020", "Juanito");
+
+      facturas.add(new Factura("Andres", lineas));
+      crearCliente(facturas, lineas, document);
+
+      lineas.clear();
+      facturas.clear();
+
+      creaTikect(lineas, "03/02/2020", "Merche");
+      creaTikect(lineas, "04/02/2020", "Juanito");
+
+      facturas.add(new Factura("Rosa", lineas));
+      crearCliente(facturas, lineas, document);
+
+
+      Source source = new DOMSource(document);
+      Result result = new StreamResult(new java.io.File(".\\Archivos\\Comedor.xml"));
+      Transformer transformer = TransformerFactory.newInstance().newTransformer();
+      transformer.transform(source, result);
+      //Result console = new StreamResult(System.out);
+      //transformer.transform(source, console);
+
+    } catch (ParserConfigurationException | TransformerConfigurationException ignored) {
+    } catch (TransformerException e) {
+      e.printStackTrace();
     }
 
-    /**
-     * Crea el fichero XML
-     */
-    private static void crearXml() {
+  }
 
-        List<Factura> facturas = new ArrayList<>();
-        List<LineaTiket> lineas = new ArrayList<>();
+  /**
+   * @param lineas Contiene lo que seria el contenido de un ticket
+   * @param fecha  Contiene la fecha del tiket
+   */
+  private static void creaTikect(List<LineaTiket> lineas, String fecha, String camarero) {
 
+    String primero = (String) primerosArray[new Random().nextInt(primerosArray.length)];
+    String segundo = (String) segundosArray[new Random().nextInt(segundosArray.length)];
+    String postre = (String) postresArray[new Random().nextInt(postresArray.length)];
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    lineas.add(
+        new LineaTiket(primeros.get(primero) + segundos.get(segundo) + postres.get(postre),
+            fecha, camarero, String.valueOf(primero),
+            String.valueOf(segundo), String.valueOf(postre)));
 
-        try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            DOMImplementation implementation = builder.getDOMImplementation();
-            Document document = implementation.createDocument(null, "Comandas", null);
-            document.setXmlVersion("1.0");
+  }
 
-            lineas.add(new LineaTiket(105, "03/02/2020", "Juanito", "Lentejas", "Guiso", "Goxua"));
-            lineas.add(new LineaTiket(15, "04/02/2020", "Juanito", "Fideos", "Lubina", "Galletas"));
-            facturas.add(new Factura("pedro",lineas));
+  static void crearCliente(List<Factura> facturas, List<LineaTiket> lineas, Document document) {
+    for (Factura factura : facturas) {
 
-            crearCliente(facturas,lineas,document);
+      //Crea y añade el nodo
+      Element cliente = document.createElement("Cliente"); //nodo cliente
 
-            lineas.clear();
-            facturas.clear();
+      cliente.setAttribute("nombre", factura.getNombre());
 
-            lineas.add(new LineaTiket(75, "05/02/2020", "Merche", "Trucha marinada", "Sopa de huevo", "Trufas de cava"));
-            lineas.add(new LineaTiket(12, "04/05/2020", "Juanito", "Judías verdes", "Bonito marinado", "Crema catalana"));
-            lineas.add(new LineaTiket(200, "07/01/2020", "Juanito", "Berenjenas agridulces", "Lomo de cerdo", "Brownie de chocolate"));
-            lineas.add(new LineaTiket(80, "09/03/2020", "Juanito", "Lentejas con calabaza", "Fajitas de ternera", "Tiramisú"));
-            facturas.add(new Factura("Juan",lineas));
-
-            crearCliente(facturas,lineas,document);
-
-            lineas.clear();
-            facturas.clear();
-
-            lineas.add(new LineaTiket(90, "09/03/2020", "Juanito", "Entrecot", "Arroz tres delicias", "mochis"));
-            facturas.add(new Factura("Andres",lineas));
-
-            crearCliente(facturas,lineas,document);
-
-            lineas.clear();
-            facturas.clear();
-
-            lineas.add(new LineaTiket(11, "03/02/2020", "Merche", "Lentejas", "Guiso", "Goxua"));
-            lineas.add(new LineaTiket(15, "04/02/2020", "Merche", "Fideos", "Lubina", "Galletas"));
-            facturas.add(new Factura("Rosa",lineas));
-
-            crearCliente(facturas,lineas,document);
+      document.getDocumentElement().appendChild(cliente); //lo añade a la raíz del documento
 
 
+      for (LineaTiket linea : lineas) {
 
-            Source source = new DOMSource(document);
-            Result result = new StreamResult(new java.io.File(".\\Archivos\\Comedor.xml"));
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.transform(source, result);
-            //Result console = new StreamResult(System.out);
-            //transformer.transform(source, console);
-
-        } catch (ParserConfigurationException | TransformerConfigurationException ignored) {
-        } catch (TransformerException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    static void CrearElemento(String datoEmple, String valor, Element raiz, Document document) {
-        Element elem = document.createElement(datoEmple); //creamos hijo
-
-        Text text = document.createTextNode(valor); //damos valor
-        raiz.appendChild(elem); //pegamos el elemento hijo a la raiz
-        elem.appendChild(text); //pegamos el valor
-    }
-
-    static void crearCliente(List<Factura> facturas, List<LineaTiket> lineas, Document document){
-        for (Factura factura : facturas) {
-
-            //Crea y añade el nodo
-            Element cliente = document.createElement("Cliente"); //nodo cliente
-
-            cliente.setAttribute("nombre", factura.getNombre());
-
-            document.getDocumentElement().appendChild(cliente); //lo añade a la raíz del documento
+        Element tiket = document.createElement("Factura");
 
 
-            for (LineaTiket linea : lineas) {
+        document.getDocumentElement().appendChild(tiket);
 
-                Element tiket = document.createElement("Factura");
-
-
-                document.getDocumentElement().appendChild(tiket);
-
-                cliente.appendChild(tiket);
+        cliente.appendChild(tiket);
 
                    /* Text text = document.createTextNode("Fecha"); //damos valor
                     cliente.appendChild(tiket); //pegamos el elemento hijo a la raiz
                     tiket.appendChild(text); //pegamos el valor*/
 
-                //se añaden los hijos al nodo raiz
-                CrearElemento("Fecha", linea.getFecha(), tiket, document);
-                CrearElemento("Camarero", linea.getCamarero(), tiket, document);
-                CrearElemento("Primero", linea.getPrimero(), tiket, document);
-                CrearElemento("Segundo", linea.getSegundo(), tiket, document);
-                CrearElemento("Postre", linea.getPostre(), tiket, document);
-                CrearElemento("Importe", String.valueOf(linea.getImporte()), tiket, document);
-            }
-
-        }
+        //se añaden los hijos al nodo raiz
+        CrearElemento("Fecha", linea.getFecha(), tiket, document);
+        CrearElemento("Camarero", linea.getCamarero(), tiket, document);
+        CrearElemento("Primero", linea.getPrimero(), tiket, document);
+        CrearElemento("Segundo", linea.getSegundo(), tiket, document);
+        CrearElemento("Postre", linea.getPostre(), tiket, document);
+        CrearElemento("Importe", String.valueOf(linea.getImporte()), tiket, document);
+      }
 
     }
+
+  }
+
+  static void CrearElemento(String datoEmple, String valor, Element raiz, Document document) {
+    Element elem = document.createElement(datoEmple); //creamos hijo
+
+    Text text = document.createTextNode(valor); //damos valor
+    raiz.appendChild(elem); //pegamos el elemento hijo a la raiz
+    elem.appendChild(text); //pegamos el valor
+  }
+
 
 }
 
